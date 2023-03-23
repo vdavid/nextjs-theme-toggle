9dc58ad85d2accdd7193282a256f9b17308d660d
David Veszelovszki <veszelovszki@gmail.com>
2023-03-24 00:55:35 +0100

    Fix it

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useTheme } from 'next-themes'
-import styles from './ThemeToggle.module.scss.d'
-import classnames from 'classnames'
+import styles from './index.module.scss'
 
-const ThemeToggle = ({hideOnSmallScreens}: {hideOnSmallScreens?: boolean}) => {
+const ThemeToggle = ({useTheme, hideOnSmallScreens}: {useTheme: () => any, hideOnSmallScreens?: boolean}) => {
     const { theme, setTheme } = useTheme()
 
-    return <div className={classnames(styles.toggle, hideOnSmallScreens && styles.hideOnSmallScreens)} title="Dark mode">
+    return <div className={[styles.toggle, hideOnSmallScreens && styles.hideOnSmallScreens].filter(x=>x).join(' ')} title="Dark mode">
         <input className={styles.input} type="checkbox" id="theme-toggle-input" defaultChecked={theme === 'dark'}
                onClick={(event) => setTheme((event.target as HTMLInputElement).checked ? 'dark' : 'light')} />
         <label htmlFor="theme-toggle-input">
